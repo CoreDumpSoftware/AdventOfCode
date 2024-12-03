50c63f8cf8e57f7dd7a83e717d30324288b8df01
Kevin Andres <kevin.andres@marcusmillichap.com>
2024-12-03 10:20:15 -0700

    D3 P1

--- AdventOfCode/Puzzles/Y2024/D3/PuzzleSolution.cs
+++ AdventOfCode/Puzzles/Y2024/D3/PuzzleSolution.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Text.RegularExpressions;
 using AdventOfCode.Services;
 
 namespace AdventOfCode.Puzzles.Y2024.D3;
@@ -11,13 +7,26 @@ public class PuzzleSolution(string input) : IPuzzleSolution
 {
 	private readonly string _input = input;
 
-	public Task<object> PartOne()
+	public async Task<object> PartOne()
 	{
-		return 0;
+		var regex = new Regex(@"mul\((?'left'\d+),(?'right'\d+)\)");
+
+		var matches = regex.Matches(_input);
+		var answer = 0l;
+
+		foreach (Match match in matches)
+		{
+			var left = int.Parse(match.Groups["left"].ToString());
+			var right = int.Parse(match.Groups["right"].ToString());
+
+			answer += left * right;
+		}
+
+		return await Task.FromResult(answer);
 	}
 
-	public Task<object> PartTwo()
+	public async Task<object> PartTwo()
 	{
-		return 0;
+		return await Task.FromResult(0);
 	}
 }
