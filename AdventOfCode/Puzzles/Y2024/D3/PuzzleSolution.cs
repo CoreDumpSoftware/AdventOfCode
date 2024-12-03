3f9cd4cb46fe31d39d7509677b1e67f790d54f93
Kevin Andres <kevin.andres@marcusmillichap.com>
2024-12-03 10:53:30 -0700

    D3 P2

--- AdventOfCode/Puzzles/Y2024/D3/PuzzleSolution.cs
+++ AdventOfCode/Puzzles/Y2024/D3/PuzzleSolution.cs
@@ -6,12 +6,43 @@ namespace AdventOfCode.Puzzles.Y2024.D3;
 public class PuzzleSolution(string input) : IPuzzleSolution
 {
 	private readonly string _input = input;
+	private static Regex mulRegex = new Regex(@"mul\((?'left'\d+),(?'right'\d+)\)");
 
 	public async Task<object> PartOne()
 	{
-		var regex = new Regex(@"mul\((?'left'\d+),(?'right'\d+)\)");
+		return await Task.FromResult(Calculate(_input));
+	}
 
-		var matches = regex.Matches(_input);
+	public async Task<object> PartTwo()
+	{
+		var i = 0;
+		var enabled = true;
+		var answer = 0l;
+
+		// Find enabled sections, perform calculations when enabled
+		var enableRegex = new Regex(@"do\(\)|don't\(\)");
+		foreach (Match enableMatch in enableRegex.Matches(_input))
+		{
+			if (enabled)
+			{
+				var preceedingText = _input.Substring(i, enableMatch.Index - i);
+				answer += Calculate(preceedingText);
+			}
+
+			if (enableMatch.Value == "don't()")
+				enabled = false;
+			else
+				enabled = true;
+
+			i = enableMatch.Index + enableMatch.Length;
+		}
+
+		return await Task.FromResult(answer);
+	}
+
+	private long Calculate(string input)
+	{
+		var matches = mulRegex.Matches(input);
 		var answer = 0l;
 
 		foreach (Match match in matches)
@@ -22,11 +53,6 @@ public class PuzzleSolution(string input) : IPuzzleSolution
 			answer += left * right;
 		}
 
-		return await Task.FromResult(answer);
-	}
-
-	public async Task<object> PartTwo()
-	{
-		return await Task.FromResult(0);
+		return answer;
 	}
 }
