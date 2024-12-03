6f0e2ea052f497580b1f1125e68973cc15e0d17c
Kevin Andres <kevin.andres@marcusmillichap.com>
2024-12-03 10:15:26 -0700

    Add D3 rigging

---
+++ AdventOfCode/Puzzles/Y2024/D3/PuzzleSolution.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AdventOfCode.Services;
+
+namespace AdventOfCode.Puzzles.Y2024.D3;
+
+public class PuzzleSolution(string input) : IPuzzleSolution
+{
+	private readonly string _input = input;
+
+	public Task<object> PartOne()
+	{
+		return 0;
+	}
+
+	public Task<object> PartTwo()
+	{
+		return 0;
+	}
+}
